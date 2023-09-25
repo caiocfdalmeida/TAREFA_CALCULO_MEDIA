5ea2cfe433242f55185f639f1b88be7428ed075c
Caio <ccardoso.judo@gmail.com>
2023-09-25 10:47:52 -0300

    EXERCICIO_MODULO_08

---
+++ TAREFA_CALCULO_MEDIA/src/TAREFA_CALCULO_MEDIA/CALCULO_MEDIA.java
@@ -0,0 +1,18 @@
+package TAREFA_CALCULO_MEDIA;
+
+public class CALCULO_MEDIA {
+	
+	public static void main (String args []) {
+		
+		int nota1 = 10;
+		int nota2 = 8;
+		int nota3 = 6;
+		int nota4 = 8;
+		
+		int mediaNotas = ((nota1 + nota2 + nota3 + nota4)/4);
+		
+		System.out.println ("A média das notas é: " + mediaNotas);
+		
+	}
+
+}
